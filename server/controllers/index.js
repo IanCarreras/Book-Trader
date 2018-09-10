894ddfe759c4d35ec9aba053202fa71420a5f8b7
Ian <mus5002@aol.com>
2018-09-09 21:54:56 -0400

    added controllers to index

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,10 @@
 const users = require('./user_controller')
+const books = require('./book_controller')
+const requests = require('./request_controller')
+
 
 module.exports = {
-  users
+  users,
+  books,
+  requests
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   var user = sequelize.define('user', {
     first_name: {
